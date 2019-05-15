3bffb4825b16ecf0846d33788d8f6c60b35a298d
leepuppychow <leepuppychow@gmail.com>
2019-05-15 06:08:14 -0600

    Adds routes to get all users and addresses

--- server/app/app.py
+++ server/app/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_migrate import Migrate 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -11,13 +12,25 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://%s:%s@%s/%s' % (
 )
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
-from models import *
+from models import User, Address, users_schema, addresses_schema
 
-@app.route("/api/v1/ping")
+@app.route("/api/v1/ping", methods=["GET"])
 def health_check():
   return jsonify(
     status="OK"
   ), 200
 
+@app.route("/api/v1/users", methods=["GET"])
+def all_users():
+  all_users = User.all()
+  result = users_schema.dump(all_users)
+  return jsonify(result.data), 200
+
+@app.route("/api/v1/addresses", methods=["GET"])
+def all_addresses():
+  all_addresses = Address.all()
+  result = addresses_schema.dump(all_addresses)
+  return jsonify(result.data), 200
