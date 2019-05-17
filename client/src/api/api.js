5a540257ede8a2e53775838758836d9a70ecf6c8
leepuppychow <leepuppychow@gmail.com>
2019-05-17 02:48:42 -0600

    Fixes odd bug with string.trim with headers using vue http

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,45 +1,50 @@
 import Vue from 'vue';
 
 const baseUrl = 'http://localhost:5000/api/v1';
-const headers = {
-  'Content-Type': 'application/json',
-};
-const authToken = sessionStorage.getItem('addressToken');
 
-export const login = body => Vue.http.post(`${baseUrl}/login`, body, headers);
-export const register = body => Vue.http.post(`${baseUrl}/register`, body, headers);
+export const login = body => Vue.http.post(`${baseUrl}/login`, body, {
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export const register = body => Vue.http.post(`${baseUrl}/register`, body, {
+  headers: { 'Content-Type': 'application/json' },
+});
+
 export const validateToken = token => Vue.http.get(`${baseUrl}/validate_token`, {
   headers: {
-    ...headers,
+    'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`,
   },
 });
+
 export const allAddresses = () => Vue.http.get(`${baseUrl}/addresses`, {
   headers: {
-    ...headers,
-    Authorization: `Bearer ${authToken}`,
+    Authorization: `Bearer ${sessionStorage.getItem('addressToken')}`,
   },
 });
+
 export const findAddress = id => Vue.http.get(`${baseUrl}/addresses/${id}`, {
   headers: {
-    ...headers,
-    Authorization: `Bearer ${authToken}`,
+    Authorization: `Bearer ${sessionStorage.getItem('addressToken')}`,
   },
 });
+
 export const createAddress = body => Vue.http.post(`${baseUrl}/addresses`, body, {
   headers: {
-    ...headers,
-    Authorization: `Bearer ${authToken}`,
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${sessionStorage.getItem('addressToken')}`,
   },
 });
+
 export const editAddress = (id, body) => Vue.http.put(`${baseUrl}/addresses/${id}`, body, {
   headers: {
-    ...headers,
-    Authorization: `Bearer ${authToken}`,
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${sessionStorage.getItem('addressToken')}`,
   },
 });
+
 export const deleteAddress = id => Vue.http.delete(`${baseUrl}/addresses/${id}`, {
   headers: {
-    Authorization: `Bearer ${authToken}`,
+    Authorization: `Bearer ${sessionStorage.getItem('addressToken')}`,
   },
 });
