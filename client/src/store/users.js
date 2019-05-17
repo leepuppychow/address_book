066bc11d787b4045018566727906ea57cc6ebb94
leepuppychow <leepuppychow@gmail.com>
2019-05-16 23:06:20 -0600

    Creates actions, mutations for users vuex module

--- client/src/store/users.js
+++ client/src/store/users.js
@@ -1,16 +1,51 @@
+import { login, register } from '../api/api';
+
 const users = {
   namespaced: true,
   state: {
-
+    currentUser: null,
+    loading: false,
+    error: null,
   },
   getters: {
 
   },
   mutations: {
-
+    setLoadingStatus(state, status) {
+      state.loading = status;
+    },
+    setError(state, err) {
+      state.error = err;
+    },
   },
   actions: {
-
+    async login({ commit }, body) {
+      try {
+        commit('setLoadingStatus', true);
+        const response = await login(body);
+        const data = await response.json();
+        const { token } = data;
+        sessionStorage.setItem('addressToken', token);
+        commit('setLoadingStatus', false);
+      } catch (err) {
+        commit('setError', err);
+      }
+    },
+    async register({ commit }, body) {
+      try {
+        commit('setLoadingStatus', true);
+        const response = await register(body);
+        const data = await response.json();
+        const { token } = data;
+        sessionStorage.setItem('addressToken', token);
+        commit('setLoadingStatus', false);
+      } catch (err) {
+        commit('setError', err);
+      }
+    },
+    logout() {
+      sessionStorage.removeItem('addressToken');
+    },
   },
 };
 
