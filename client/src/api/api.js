2dd9720177199c92a5c588dfc2bc2d65821b36ea
leepuppychow <leepuppychow@gmail.com>
2019-05-17 02:04:07 -0600

    Able to display all addresses from server on dashboard

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,6 +4,7 @@ const baseUrl = 'http://localhost:5000/api/v1';
 const headers = {
   'Content-Type': 'application/json',
 };
+const authToken = sessionStorage.getItem('addressToken');
 
 export const login = body => Vue.http.post(`${baseUrl}/login`, body, headers);
 export const register = body => Vue.http.post(`${baseUrl}/register`, body, headers);
@@ -11,5 +12,34 @@ export const validateToken = token => Vue.http.get(`${baseUrl}/validate_token`, 
   headers: {
     ...headers,
     Authorization: `Bearer ${token}`,
+  },
+});
+export const allAddresses = () => Vue.http.get(`${baseUrl}/addresses`, {
+  headers: {
+    ...headers,
+    Authorization: `Bearer ${authToken}`,
+  },
+});
+export const findAddress = id => Vue.http.get(`${baseUrl}/addresses/${id}`, {
+  headers: {
+    ...headers,
+    Authorization: `Bearer ${authToken}`,
+  },
+});
+export const createAddress = body => Vue.http.post(`${baseUrl}/addresses`, body, {
+  headers: {
+    ...headers,
+    Authorization: `Bearer ${authToken}`,
+  },
+});
+export const editAddress = (id, body) => Vue.http.put(`${baseUrl}/addresses/${id}`, body, {
+  headers: {
+    ...headers,
+    Authorization: `Bearer ${authToken}`,
+  },
+});
+export const deleteAddress = id => Vue.http.delete(`${baseUrl}/addresses/${id}`, {
+  headers: {
+    Authorization: `Bearer ${authToken}`,
   },
 });
--- client/src/store/addresses.js
+++ client/src/store/addresses.js
@@ -1,16 +1,44 @@
+import { 
+  allAddresses,
+  findAddress,
+  deleteAddress,
+  createAddress,
+  editAddress,
+} from '../api/api';
+
 const addresses = {
   namespaced: true,
   state: {
-
+    all: [],
+    loading: false,
+    error: '',
   },
   getters: {
 
   },
   mutations: {
-
+    setLoadingStatus(state, status) {
+      state.loading = status;
+    },
+    setError(state, err) {
+      state.error = err;
+    },
+    setAddresses(state, payload) {
+      state.all = payload;
+    },
   },
   actions: {
-
+    async getAllAddresses({ commit }) {
+      try {
+        commit('setLoadingStatus', true);
+        const response = await allAddresses();
+        const payload = await response.json();
+        commit('setAddresses', payload);
+        commit('setLoadingStatus', false);
+      } catch (_err) {
+        commit('setError', 'Error getting all addresses');
+      }
+    },
   },
 };
 
