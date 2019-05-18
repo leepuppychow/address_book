db2a3968b36d9519be4f01ab017284414989b22f
leepuppychow <leepuppychow@gmail.com>
2019-05-18 12:42:11 -0600

    Can selectContact and display on R side of dashboard

--- client/src/components/ContactCard.vue
+++ client/src/components/ContactCard.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="contact-card">
-
+    <div v-if="selectedContact">
+      <p>{{ selectedContact.first_name }} {{ selectedContact.last_name }}</p>
+      <p>{{ selectedContact.street }}</p>
+      <p>{{ selectedContact.city }}, {{ selectedContact.state }} {{ selectedContact.zip }}</p>
+      <p>{{ selectedContact.phone }}</p>
+      <p>{{ selectedContact.email }}</p>
+    </div>
   </div>  
 </template>
 
 <script>
 export default {
-
+  computed: {
+    selectedContact() {
+      return this.$store.state.addresses.selectedContact;
+    }
+  },
 }
 </script>
 
@@ -14,7 +24,7 @@ export default {
 .contact-card {
   height: 100%;
   width: 50%;
-  border: solid 1px black;
+  border: solid 1px $app-blue;
 }
 
 </style>
--- client/src/components/ContactList.vue
+++ client/src/components/ContactList.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="all-contacts">
     <h3>All Contacts</h3>
-    <div class="contact-row">
-      <p 
-        :key="a.id"
-        v-for="a in addresses"
-        @click="selectContact(a.id)"
-      >
-        {{ a.first_name }} {{ a.last_name }}
-      </p>
-    </div>
+    <p
+      class="contact-row"
+      :key="a.id"
+      v-for="a in addresses"
+      @click="selectContact(a.id)"
+    >
+      {{ a.first_name }} {{ a.last_name }}
+    </p>
   </div>
 </template>
 
@@ -22,7 +21,7 @@ export default {
   },
   methods: {
     selectContact(addressId) {
-      debugger
+      this.$store.dispatch('addresses/selectContact', addressId);
     },
   },
 }
@@ -32,7 +31,10 @@ export default {
 #all-contacts {
   height: 100%;
   width: 50%;
-  border: solid 1px black;
+  border: solid 1px $app-blue;
+}
+.contact-row {
+  cursor: pointer;
 }
 
 </style>
